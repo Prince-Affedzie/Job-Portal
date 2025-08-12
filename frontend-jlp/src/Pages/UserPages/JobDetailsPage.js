34bba0ab0c422f2c81ffb88988a010d27fc6b833
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2025-08-12 10:22:08 +0000

    refined job details

--- frontend-jlp/src/Pages/UserPages/JobDetailsPage.js
+++ frontend-jlp/src/Pages/UserPages/JobDetailsPage.js
@@ -252,14 +252,14 @@ const JobDetails = () => {
                                         </span>
                                     </div>
                                     
-                                    {!hasApplied && !isDeadlinePassed && !showForm && (
+                                   {/* {!hasApplied && !isDeadlinePassed && !showForm && (
                                         <button 
                                             onClick={() => setShowForm(true)}
                                             className="mt-6 px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition-colors shadow-sm"
                                         >
                                             Apply Now
                                         </button>
-                                    )}
+                                    )}*/}
                                 </div>
                             </div>
                         </div>
